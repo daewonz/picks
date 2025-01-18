04833bc5429be4baae6a99c88100709c460a8b09
leedaewon <daewonz@uracle.co.kr>
2025-01-18 21:48:53 +0900

    picks init

---
+++ src/test/java/com/shop/picks/PicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.picks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
